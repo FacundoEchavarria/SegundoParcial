692c282f0d2a4c9947320ba54141fe4bcea83715
Eduardo Benjamin Lopez Avila <92894636+benlopez00@users.noreply.github.com>
2025-11-11 23:30:09 -0300

    spinner + wip

--- js/detalle.js
+++ js/detalle.js
@@ -1,42 +1,39 @@
 // FUNCION AL CARGAR ELEMENTOS DE PAGINA DETALLE
-document.addEventListener('DOMContentLoaded', () => {
-  // BUSCA EL ID DE LOS PARAMETROS DE LA URL
-  const parametrosURL = new URLSearchParams(window.location.search);
-  const productoId = parametrosURL.get('id');
-  const contenedorDetalle = document.getElementById('contenedor-detalle');
+document.addEventListener("DOMContentLoaded", () => {
+	// BUSCA EL ID DE LOS PARAMETROS DE LA URL
+	const parametrosURL = new URLSearchParams(window.location.search);
+	const productoId = parametrosURL.get("id");
+	const contenedorDetalle = document.getElementById("contenedor-detalle");
 
-  if (!productoId || !contenedorDetalle) {
-    contenedorDetalle.innerHTML =
-      '<h2 class="text-danger">Error: Producto no encontrado.</h2>';
-    return;
-  }
+	if (!productoId || !contenedorDetalle) {
+		contenedorDetalle.innerHTML =
+			'<h2 class="text-danger">Error: Producto no encontrado.</h2>';
+		return;
+	}
 
-  // FUNCION QUE SE EJECUTAL AL CARGAR COMPONENTES
-  const iniciarDetalle = async () => {
-    try {
-      contenedorDetalle.innerHTML =
-        '<div class="col-12 text-center text-muted">Cargando productos destacados...</div>';
+	// FUNCION QUE SE EJECUTAL AL CARGAR COMPONENTES
+	const iniciarDetalle = async () => {
+		try {
+			console.log("Obteniendo producto...");
+			// PETICION A LA API
+			productoEncontrado = await api.obtenerProductoId(productoId);
+			console.log("Producto obtenido correctamente.");
 
-      console.log('Obteniendo producto...');
-      // PETICION A LA API
-      productoEncontrado = await api.obtenerProductoId(productoId);
-      console.log('Producto obtenido correctamente.');
+			// RENDERIZACION DE LA PAGINA
+			render.crearDetalleProducto(productoEncontrado, contenedorDetalle);
+			const botonAgregar = document.getElementById("btn-agregar-detalle");
 
-      // RENDERIZACION DE LA PAGINA
-      render.crearDetalleProducto(productoEncontrado, contenedorDetalle);
-      const botonAgregar = document.getElementById('btn-agregar-detalle');
+			//AGREGAR EVENTO AL BOTON DEL CARRITO
+			botonAgregar.addEventListener("click", () => {
+				carrito.agregar(productoEncontrado);
+				render.mostrarToast("Producto agregado al carrito", "succes");
+			});
+		} catch (error) {
+			// MANEJO DE ERRORES
+			console.log(`Error al buscar producto: ${error.message}`);
+			render.mostarError(error, contenedorDetalle);
+		}
+	};
 
-      //AGREGAR EVENTO AL BOTON DEL CARRITO
-      botonAgregar.addEventListener('click', () => {
-        carrito.agregar(productoEncontrado);
-        render.mostrarToast('Producto agregado al carrito', 'succes');
-      });
-    } catch (error) {
-      // MANEJO DE ERRORES
-      console.log(`Error al buscar producto: ${error.message}`);
-      render.mostarError(error, contenedorDetalle);
-    }
-  };
-
-  iniciarDetalle();
+	iniciarDetalle();
 });
--- js/main.js
+++ js/main.js
@@ -1,45 +1,50 @@
 // FUNCION AL CARGAR ELEMENTOS DE PAGINA INICIO
-document.addEventListener('DOMContentLoaded', () => {
-  const productsContainer = document.getElementById('productos-container');
-  let todosLosProductos = [];
+document.addEventListener("DOMContentLoaded", () => {
+	const productsContainer = document.getElementById("productos-container");
+	let todosLosProductos = [];
 
-  // FUNCION DE PETICION A LA API Y RENDERIZAR PRODUCTOS
-  const iniciarIndex = async () => {
-    try {
-      productsContainer.innerHTML =
-        '<div class="col-12 text-center text-muted">Cargando productos destacados...</div>';
+	// FUNCION DE PETICION A LA API Y RENDERIZAR PRODUCTOS
+	const iniciarIndex = async () => {
+		try {
+			console.log("Obteniendo productos...");
+			todosLosProductos = await api.obtenerProductos();
+			console.log("Productos obtenidos correctamente.");
 
-      console.log('Obteniendo productos...');
-      todosLosProductos = await api.obtenerProductos();
-      console.log('Productos obtenidos correctamente.');
+			render.crearProductosDestacados(
+				todosLosProductos,
+				productsContainer
+			);
+			agregarListenersBoton();
+		} catch (error) {
+			console.log(`Error al pedir productos: ${error.message}`);
+			render.mostarError(error, productsContainer);
+		}
+	};
 
-      render.crearProductosDestacados(todosLosProductos, productsContainer);
-      agregarListenersBoton();
-    } catch (error) {
-      console.log(`Error al pedir productos: ${error.message}`);
-      render.mostarError(error, productsContainer);
-    }
-  };
+	// FUNCIOND DE INICILIZACION DE LISTENERS
+	const agregarListenersBoton = () => {
+		const botonesAgregar = document.querySelectorAll(".btn-agregar");
+		botonesAgregar.forEach((boton) => {
+			boton.addEventListener("click", (evento) => {
+				// Obtiene el ID del producto del atributo data-product-id
+				const productoId = parseInt(
+					evento.currentTarget.dataset.productId
+				);
+				if (!isNaN(productoId)) {
+					const productoEncontrado = todosLosProductos.find(
+						(p) => p.id === productoId
+					);
+					carrito.agregar(productoEncontrado);
+					render.mostrarToast(
+						"Producto agregado al carrito",
+						"succes"
+					);
+				} else {
+					console.error("Error: Producto no encontrado.");
+				}
+			});
+		});
+	};
 
-  // FUNCIOND DE INICILIZACION DE LISTENERS
-  const agregarListenersBoton = () => {
-    const botonesAgregar = document.querySelectorAll('.btn-agregar');
-    botonesAgregar.forEach((boton) => {
-      boton.addEventListener('click', (evento) => {
-        // Obtiene el ID del producto del atributo data-product-id
-        const productoId = parseInt(evento.currentTarget.dataset.productId);
-        if (!isNaN(productoId)) {
-          const productoEncontrado = todosLosProductos.find(
-            (p) => p.id === productoId
-          );
-          carrito.agregar(productoEncontrado);
-          render.mostrarToast('Producto agregado al carrito', 'succes');
-        } else {
-          console.error('Error: Producto no encontrado.');
-        }
-      });
-    });
-  };
-
-  iniciarIndex();
+	iniciarIndex();
 });
